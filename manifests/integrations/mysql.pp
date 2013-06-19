5f2a7abd9d702788a6a205fa243a0b7f892096f8
Kyle Campos <kyle.campos@gmail.com>
2013-06-19 16:17:45 -0700

    Add mysql integration

---
+++ manifests/integrations/mysql.pp
@@ -0,0 +1,43 @@
+# Class: datadog
+#
+# This class contains the agent installation mechanism for the Datadog module
+#
+# Parameters:
+#   $api_key:
+#       Your DataDog API Key. Please replace with your key value
+#   $puppet_run_reports
+#       Will send results from your puppet agent runs back to the datadog service
+#   $puppetmaster_user
+#       Will chown the api key used by the report processor to this user.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# include datadog
+#
+# or
+#
+# class{'datadog': api_key => 'your key'}
+#
+#
+class datadog::integrations::mysql(
+  $host,
+  $password,
+  $user = 'datadog',
+  $tags = [],
+  $replication = '0',
+  $galera_cluster = '1'
+) inherits datadog::params {
+
+  file { "${conf_dir}/mysql.yaml":
+    ensure  => file,
+    owner   => $dd_user,
+    group   => $dd_group,
+    mode    => 0644,
+    content => template('datadog/mysql.yaml.erb'),
+    notify  => Service[$service_name]
+  }
+}
